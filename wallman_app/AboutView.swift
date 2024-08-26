dd0781a87b089850d726a96c41de68f9b29ebf0d
putgeminmouth <4316458+putgeminmouth@users.noreply.github.com>
2024-08-25 21:20:41 -0400

    Add version to about window

--- wallman_app/AboutView.swift
+++ wallman_app/AboutView.swift
@@ -5,14 +5,18 @@ import OSLog
 struct AboutView: View {
     let logger = Logger(subsystem: "Wallman", category: "About")
     var body: some View {
+        let bundleVersion = Bundle.main.infoDictionary?["CFBundleVersion"] as? String ?? "unknown"
+        let version = "1.\(bundleVersion)"
+
         VStack {
             Image("AboutImage")
                 .resizable()
                 .scaledToFit()
                 .frame(width: 500, height: 500)
             Text("WallMan: Wallpaper Manager")
+            Text("Version \(version)")
             Text("[Github](https://github.com/putgeminmouth/wallman)")
         }
-            .padding(20)
+        .frame(minHeight: 570)
     }
 }
