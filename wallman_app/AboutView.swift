a6986b7db4749a43bf4852d89ddcf3755a6b3b52
putgeminmouth <4316458+putgeminmouth@users.noreply.github.com>
2024-08-25 21:20:41 -0400

    Resize about window

--- wallman_app/AboutView.swift
+++ wallman_app/AboutView.swift
@@ -5,14 +5,17 @@ import OSLog
 struct AboutView: View {
     let logger = Logger(subsystem: "Wallman", category: "About")
     var body: some View {
+        let version = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String
+        
         VStack {
             Image("AboutImage")
                 .resizable()
                 .scaledToFit()
                 .frame(width: 500, height: 500)
             Text("WallMan: Wallpaper Manager")
+            Text("Version \(version ?? "unknown")")
             Text("[Github](https://github.com/putgeminmouth/wallman)")
         }
-            .padding(20)
+        .frame(minHeight: 570)
     }
 }
